eda5dc20a02ff4304d7725750eab7bc220cafe43
abrigoni <brian.rigoni1@gmail.com>
2024-07-07 12:56:11 +0200

    feat(auth): sign in method

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,10 +1,21 @@
+import { SignInSchema } from "@/components/auth/sign-in-form";
 import { SignUpSchema } from "@/components/auth/sign-up-form";
 
 export default class UserService {
   static async createAccount(body: SignUpSchema) {
     try {
-      const user = await fetch('http://localhost:8000/api/users/register', { method: 'POST', body: JSON.stringify(body) });
-      console.log(user);
+      const res = await fetch('http://localhost:8000/api/users/register', { method: 'POST', body: JSON.stringify(body) });
+      const response = res.json();
+      console.log(response);
+    } catch (err) {
+      return err;
+    }
+  }
+  static async signIn(body: SignInSchema) {
+    try {
+      const res = await fetch('http://localhost:8000/api/users/sign-in', { method: 'POST', body: JSON.stringify(body) });
+      const response = res.json();
+      console.log(response);
     } catch (err) {
       return err;
     }
