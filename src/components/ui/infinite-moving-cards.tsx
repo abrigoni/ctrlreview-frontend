58f7dab721d944d1d923ca3cef7cdb6b8d5e41b6
abrigoni <brian.rigoni1@gmail.com>
2025-01-19 19:27:23 +0100

    card change

--- src/components/ui/infinite-moving-cards.tsx
+++ src/components/ui/infinite-moving-cards.tsx
@@ -87,10 +87,10 @@ export const InfiniteMovingCards = ({
       >
         {items.map((item, idx) => (
           <li
-            className="w-[350px] max-w-full relative rounded-2xl border border-b-0 flex-shrink-0 border-slate-700 px-8 py-6 md:w-[450px]"
+            className="w-[350px] max-w-full relative rounded-2xl border flex-shrink-0 border-slate-700 px-8 py-6 md:w-[450px]"
             style={{
               background:
-                "linear-gradient(180deg, var(--slate-800), var(--slate-900)",
+                "linear-gradient(180deg, rgba(138, 110, 255, 0.1), rgba(99, 61, 255, 0.02)",
             }}
             key={item.name}
           >
